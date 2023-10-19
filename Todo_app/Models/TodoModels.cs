ab87c9209c25e839ac95e69cac51a36ee4446ab8
artursab <arturs.belavskis@gmail.com>
2023-10-19 11:04:15 +0200

    Initial commit

---
+++ Todo_app/Models/TodoModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_app.Models
+{
+     class TodoModels
+    {
+
+            public DateTime CreationDate { get; set; } = DateTime.Now;
+
+            private bool _isDone;
+            private string _text;
+
+            public bool IsDone
+            {
+                get { return _isDone; }
+                set { _isDone = value; }
+            }
+
+            public string Text
+            {
+                get { return _text; }
+                set { _text = value; }
+            }
+
+
+        }
+    }
+
